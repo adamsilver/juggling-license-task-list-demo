1120d67fc249a483d939aab93090231b7b024794
Adam Silver <adambsilver@gmail.com>
2023-10-09 11:27:57 +0100

    Create account flow

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,26 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+router.post('/has-account', (req, res) => {
+  if(req.session.data.new.hasAccount == 'No') {
+    res.redirect('/can-juggle')
+  } else {
+    res.redirect('/email-address')
+  }
+})
+
+router.post('/can-juggle', (req, res) => {
+  if(req.session.data.new.canJuggle == 'No') {
+    res.redirect('/not-eligible')
+  } else {
+    res.redirect('/email-address')
+  }
+})
+
+router.post('/email-address', (req, res) => {
+  res.redirect('/code')
+})
+
+router.post('/code', (req, res) => {
+  res.redirect('/application')
+})
