05df0baa5112ffa1c252c66ae4d6c83b20d89afa
Adam Silver <adambsilver@gmail.com>
2023-10-16 11:48:59 +0100

    Add progress bar

--- app/routes/application/show.js
+++ app/routes/application/show.js
@@ -1,7 +1,22 @@
 module.exports = router => {
 
   router.get('/application', (req, res) => {
-    res.render('application/show')
+    let sections = [
+      'personalDetails',
+      'experience',
+      'evidence'
+    ]
+
+    let completedSectionsCount = 0
+    for(const section of sections) {
+      if(req.session.data[section] && req.session.data[section].status == 'Completed') {
+        completedSectionsCount++
+      }
+    }
+
+    res.render('application/show', {
+      completedSectionsCount
+    })
   })
 
 }
