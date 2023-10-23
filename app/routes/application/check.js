c6e489aad08b596c481c1c632e6ee775bac135d7
Adam Silver <adambsilver@gmail.com>
2023-10-23 08:43:02 +0100

    Add page to view submitted application

--- app/routes/application/check.js
+++ app/routes/application/check.js
@@ -1,6 +1,7 @@
 module.exports = router => {
 
   router.post('/application/check', (req, res) => {
+    req.session.data.sentDate = new Date().toISOString()
     res.redirect('/application/confirmation')
   })
 
--- app/routes/application/show.js
+++ app/routes/application/show.js
@@ -1,22 +1,28 @@
 module.exports = router => {
 
   router.get('/application', (req, res) => {
-    let sections = [
-      'personalDetails',
-      'experience',
-      'evidence'
-    ]
 
-    let completedSectionsCount = 0
-    for(const section of sections) {
-      if(req.session.data[section] && req.session.data[section].status == 'Completed') {
-        completedSectionsCount++
+    if(req.session.data.sentDate) {
+      res.render('application/show--submitted')
+    } else {
+      let sections = [
+        'personalDetails',
+        'experience',
+        'evidence'
+      ]
+
+      let completedSectionsCount = 0
+      for(const section of sections) {
+        if(req.session.data[section] && req.session.data[section].status == 'Completed') {
+          completedSectionsCount++
+        }
       }
+
+      res.render('application/show', {
+        completedSectionsCount
+      })
     }
 
-    res.render('application/show', {
-      completedSectionsCount
-    })
   })
 
 }
