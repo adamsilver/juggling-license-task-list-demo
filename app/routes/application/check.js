40400b161ce545c7ad2fd5721b734a26905bb94b
Adam Silver <adambsilver@gmail.com>
2023-10-24 20:27:27 +0100

    Add error states

--- app/routes/application/check.js
+++ app/routes/application/check.js
@@ -3,28 +3,70 @@ const appHelper = require('../../helpers/application-helper')
 module.exports = router => {
 
   router.get('/application/check', (req, res) => {
-    let personalDetailsFirstUnansweredQuestion = appHelper.getFirstUnansweredQuestionFromPersonalDetails(req.session.data)
-    if(!personalDetailsFirstUnansweredQuestion && req.session.data.personalDetails.status != 'Completed') {
-      personalDetailsFirstUnansweredQuestion = 'check'
+    let personalDetailsPage = appHelper.getFirstUnansweredQuestionFromPersonalDetails(req.session.data)
+    if(!personalDetailsPage && req.session.data.personalDetails.status != 'Completed') {
+      personalDetailsPage = 'check'
     }
-    let experienceFirstUnansweredQuestion = appHelper.getFirstUnansweredQuestionFromExperience(req.session.data)
-    if(!experienceFirstUnansweredQuestion && req.session.data.experience.status != 'Completed') {
-      experienceFirstUnansweredQuestion = 'check'
+    let experienceFirstPage = appHelper.getFirstUnansweredQuestionFromExperience(req.session.data)
+    if(!experienceFirstPage && req.session.data.experience.status != 'Completed') {
+      experienceFirstPage = 'check'
     }
-    let evidenceFirstUnansweredQuestion = appHelper.getFirstUnansweredQuestionFromEvidence(req.session.data)
-    if(!evidenceFirstUnansweredQuestion && req.session.data.evidence.status != 'Completed') {
-      evidenceFirstUnansweredQuestion = 'check'
+    let evidencePage = appHelper.getFirstUnansweredQuestionFromEvidence(req.session.data)
+    if(!evidencePage && req.session.data.evidence.status != 'Completed') {
+      evidencePage = 'check'
     }
+
+    let errorsList = []
+    if(req.flash('error') == 'Errors detected') {
+      if(personalDetailsPage) {
+        errorsList.push({
+          href: '#app-personal-details',
+          text: 'You must complete personal details before you can send your application'
+        })
+      }
+      if(experienceFirstPage) {
+        errorsList.push({
+          href: '#app-experience',
+          text: 'You must complete juggling experience before you can send your application'
+        })
+      }
+      if(evidencePage) {
+        errorsList.push({
+          href: '#app-evidence',
+          text: 'You must complete evidence before you can send your application'
+        })
+      }
+    }
+
     res.render('application/check', {
-      personalDetailsFirstUnansweredQuestion,
-      experienceFirstUnansweredQuestion,
-      evidenceFirstUnansweredQuestion
+      personalDetailsPage,
+      experienceFirstPage,
+      evidencePage,
+      errorsList
     })
   })
 
   router.post('/application/check', (req, res) => {
-    req.session.data.sentDate = new Date().toISOString()
-    res.redirect('/application/confirmation')
+    let personalDetailsPage = appHelper.getFirstUnansweredQuestionFromPersonalDetails(req.session.data)
+    if(!personalDetailsPage && req.session.data.personalDetails.status != 'Completed') {
+      personalDetailsPage = 'check'
+    }
+    let experienceFirstPage = appHelper.getFirstUnansweredQuestionFromExperience(req.session.data)
+    if(!experienceFirstPage && req.session.data.experience.status != 'Completed') {
+      experienceFirstPage = 'check'
+    }
+    let evidencePage = appHelper.getFirstUnansweredQuestionFromEvidence(req.session.data)
+    if(!evidencePage && req.session.data.evidence.status != 'Completed') {
+      evidencePage = 'check'
+    }
+
+    if( personalDetailsPage || experienceFirstPage || evidencePage ) {
+      req.flash('error', 'Errors detected')
+      res.redirect(`/application/check`)
+    } else {
+      req.session.data.sentDate = new Date().toISOString()
+      res.redirect('/application/confirmation')
+    }
   })
 
 }
